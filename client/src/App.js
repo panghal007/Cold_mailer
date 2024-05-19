b2b4cc7d35ec1b4175b401eaea6113ea15fc27e6
UNIQUE PANGHAL <panghalunique@gmail.com>
2024-05-19 22:30:42 +0530

    frontend changes integrated

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,9 @@ import FlowChart from './components/FlowChart';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Email Marketing Flowchart</h1>
+    
       <FlowChart />
-    </div>
+    
   );
 }
 
